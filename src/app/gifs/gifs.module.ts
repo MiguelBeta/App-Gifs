f5bb5a5a65565287ad1ed2e38aa76738e4bd530a
Miguel <workingbeta01@gmail.com>
2024-01-30 18:49:36 -0500

    App ya realizar busqueda de gifs en la web

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
-import { SerchBoxComponent } from './components/search-box/sear-box.component';
+import { SerchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
@@ -9,6 +10,7 @@ import { SerchBoxComponent } from './components/search-box/sear-box.component';
   declarations: [
     HomePageComponent,
     SerchBoxComponent,
+    CardListComponent,
   ],
   imports: [
     CommonModule,
